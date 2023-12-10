f42efe83d53ce4fa378cc8a33b643bc6cfa09b8e
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-12-10 20:11:18 +0900

    インスタンス化のみに修正

--- kadai_017/KatoExec_Chapter17.java
+++ kadai_017/KatoExec_Chapter17.java
@@ -3,22 +3,13 @@ package kadai_017;
 public class KatoExec_Chapter17 {
 
 	public static void main(String[] args) {
-		KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
-        taro.setGivenName();
-        Kato_Chapter17.execIntroduce();
-        taro.eachIntroduce();
+	KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
         System.out.println();
         
         KatoIchiro_Chapter17 ichiro = new KatoIchiro_Chapter17();
-        ichiro.setGivenName();
-        Kato_Chapter17.execIntroduce();
-        ichiro.eachIntroduce();
         System.out.println();
         
         KatoHanako_Chapter17 hanako = new KatoHanako_Chapter17();
-        hanako.setGivenName();
-        Kato_Chapter17.execIntroduce();
-        hanako.eachIntroduce();
 	}
 
 }
