db8514d3f741c3222b34e620215820c58735118b
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-12-10 20:12:54 +0900

    コンストラクタの追加

--- kadai_017/KatoTaro_Chapter17.java
+++ kadai_017/KatoTaro_Chapter17.java
@@ -1,13 +1,18 @@
 package kadai_017;
 
 public class KatoTaro_Chapter17 extends Kato_Chapter17 {
-    private String message = "私はJavaが得意です";
+
+    KatoTaro_Chapter17() {
+        setGivenName();
+        super.execIntroduce();
+        eachIntroduce();
+    }
     
     public void setGivenName() {  //名を表すフィールドの値をセットする
         givenName = "太郎";
     }
     
     public void eachIntroduce() {
-        System.out.println(this.message);
+        System.out.println("私はJavaが得意です");
     }
 }
