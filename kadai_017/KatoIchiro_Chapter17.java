18187068bb9c6910f599635a4b4c0a8bde256af0
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-12-10 20:13:47 +0900

    コンストラクタの追加

--- kadai_017/KatoIchiro_Chapter17.java
+++ kadai_017/KatoIchiro_Chapter17.java
@@ -1,14 +1,19 @@
 package kadai_017;
 
 public class KatoIchiro_Chapter17 extends Kato_Chapter17 {
-    private String message =  "好きな食べ物はリンゴです";
+
+    KatoIchiro_Chapter17() {
+        setGivenName();
+        super.execIntroduce();
+        eachIntroduce();
+    }
     
     public void setGivenName() {  //名を表すフィールドの値をセットする
         givenName = "一郎";
     }
     
     public void eachIntroduce() {
-        System.out.println(this.message);
+        System.out.println("好きな食べ物はリンゴです");
     }
 
 }
