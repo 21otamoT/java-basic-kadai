d7acefb0816c44621ba5c784c8fa487f020981b9
21otamoT <135295588+21otamoT@users.noreply.github.com>
2023-12-10 20:14:43 +0900

    コンストラクタの追加

--- kadai_017/KatoHanako_Chapter17.java
+++ kadai_017/KatoHanako_Chapter17.java
@@ -1,14 +1,18 @@
 package kadai_017;
 
 public class KatoHanako_Chapter17 extends Kato_Chapter17 {
-    private String message = "趣味は読書です";
+    
+    KatoHanako_Chapter17() {
+        setGivenName();
+        super.execIntroduce();
+        eachIntroduce();
+    }
     
     public void setGivenName() {  //名を表すフィールドの値をセットする
         givenName = "花子";
     }
     
     public void eachIntroduce() {
-        System.out.println(this.message);
+        System.out.println("趣味は読書です");
     }
-    
 }
