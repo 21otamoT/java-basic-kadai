88ab37db4bcecec0cdea15d16ff791ec7236d41c
hirose <yukihio76@gmail.com>
2024-01-20 01:36:45 +0900

    kadai_10の追加

---
+++ kadai_010/Scores_Chapter10.java
@@ -0,0 +1,68 @@
+package kadai_010;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Scores_Chapter10 {
+	public static void main(String[] args) {
+	    Connection con = null;
+	    Statement statement = null;
+	    
+	    try {
+	    	// データベースに接続
+	    	con = DriverManager.getConnection(
+	    			"jdbc:mysql://localhost/challenge_java",
+	    			"root",
+	    			"yukihiro-1220");
+	    	
+	    	System.out.println("データベース接続成功:"+con);
+	    	
+	    	// SQLクエリを準備
+            statement = con.createStatement();
+            String sql = "UPDATE scores SET score_math=95,score_english=80 WHERE id = 1;";
+            
+            // SQLクエリを実行（DBMSに送信）
+	    	System.out.println("レコード更新を実行します");
+	    	int rowCnt = statement.executeUpdate(sql);
+		    System.out.println(rowCnt + "件のレコードが更新されました");
+		    
+		    // SQLクエリを準備
+		    sql = "SELECT * FROM scores";
+		    
+		    // SQLクエリを実行（DBMSに送信）
+		    ResultSet table = statement.executeQuery(sql);
+		    int scoreMath = 0; 
+		    String colomn = "score_math";
+		    String order = "SELECT * FROM scores ORDER BY "+colomn+" DESC";
+		    System.out.println("数学・英語の点数が高い順に並べ替えました");
+		    while(table.next()) {
+		    	int id = table.getInt("id");
+		    	System.out.println(id);
+		    	String name = table.getString("name");
+		    	int scoreMath2 = table.getInt("score_math");
+		    	int scoreEn = table.getInt("score_english");
+		    	if(scoreMath == scoreMath2) {
+		    		colomn = "score_english";
+		    	}
+		    	scoreMath = scoreMath2;
+//		    	statement.executeQuery(order);	
+		    	System.out.println(table.getRow()+"件目：生徒ID="+id+"／氏名="+name+"／数学="+scoreMath2+"／英語="+scoreEn);
+		    }
+	    }
+	    catch(SQLException e) {
+            System.out.println("エラー発生：" + e.getMessage());
+            }
+	    finally {
+	    	// 使用したオブジェクトを解放
+            if( statement != null ) {
+                try { statement.close(); } catch(SQLException ignore) {}
+            }
+            if( con != null ) {
+                try { con.close(); } catch(SQLException ignore) {}
+            }
+	    }
+		}
+}
