d1f58f85f4f9785c3f6904395f8f2b6b6abab967
21otamoT <135295588+21otamoT@users.noreply.github.com>
2024-01-25 00:40:06 +0900

    標準入力から受け取った自分の手を変数に代入しそれをメソッドの引数にするように変更

--- kadai_024/JyankenExec_Chapter24.java
+++ kadai_024/JyankenExec_Chapter24.java
@@ -7,7 +7,8 @@ public class JyankenExec_Chapter24 {
 	public static void main(String[] args) throws Exception {
 		Jyanken_Chapter24 janken = new Jyanken_Chapter24();
 		Scanner sc = new Scanner(System.in);
-		String myChoice = janken.getMyChoice(sc.next());
+		String inputChoice = sc.next();
+		String myChoice = janken.getMyChoice(inputChoice);
 		String enemy = janken.getRandom();
 		janken.playGame(myChoice, enemy);
 		sc.close();
