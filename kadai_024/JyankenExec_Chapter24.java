236d1fc1e4956dfd75aef7309fadcbfbfb730459
hirose <yukihio76@gmail.com>
2024-01-28 23:30:45 +0900

    プログラムの修正

--- kadai_024/JyankenExec_Chapter24.java
+++ kadai_024/JyankenExec_Chapter24.java
@@ -7,10 +7,17 @@ public class JyankenExec_Chapter24 {
 	public static void main(String[] args) throws Exception {
 		Jyanken_Chapter24 janken = new Jyanken_Chapter24();
 		Scanner sc = new Scanner(System.in);
-		String inputChoice = sc.next();
-		String myChoice = janken.getMyChoice(inputChoice);
-		String enemy = janken.getRandom();
-		janken.playGame(myChoice, enemy);
+		
+		while(sc.hasNext()) {
+			String inputChoice = sc.next();
+			String myChoice = janken.getMyChoice(inputChoice);
+			String enemy = janken.getRandom();
+			janken.playGame(myChoice, enemy);
+			
+			if(!myChoice.contains("r,s,pのどれかを入力してください")) {
+				break;
+			}
+		}
 		sc.close();
 	}
 
